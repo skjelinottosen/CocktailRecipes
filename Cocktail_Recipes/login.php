d01b3a66159bc5709eeb882a4a1bb8b3181f5c66
skjelinottosen <32384597+skjelinottosen@users.noreply.github.com>
2020-08-02 13:14:02 +0200

    Update login.php

--- Cocktail_Recipes/login.php
+++ Cocktail_Recipes/login.php
@@ -47,52 +47,49 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 <!DOCTYPE html>
 <html>
-
-<head>
-   <link rel="stylesheet" href="index.css">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-
-<body>
-   <div id="mastergrid">
-      <nav class="nav">
-         <ul>
-            <?php
-            if ($loggedIn == False) {
-               echo
-                  '<li class="right">
-                     <button type="button"class="button" onclick=location.href="login.php">Login</button>
-                     <button type="button" class="button" onclick=location.href="signup.php">Sign Up</button>
-                  </li>';
-            } else {
-               echo
-                  '<li class="right" >
-                     <label> Logged in as' . ' <span id="display-username">' . $_SESSION['user'] . '</span></label>
-                     <button type="button" class="button"><a href="./logout.php">Logout</a></button>
-                  </li>';
-               header("Location: ./index.php?");
-            }
-            ?>
-         </ul>
-      </nav>
-      <!--Sign up form-->
-      <main class="main">
-         <img class="icon" src="./images/Icon.png" alt="icon">
-         <section class="outerBox">
-            <h2>Login</h2>
-            <h3>Cocktail Recipes</h3>
-            <section class="innerBox">
-               <form action="" method="post">
-                  <label>Email</label>
-                  <input class="a-input" type="email" name="email" Placeholder="Email" pattern='^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$' required />
-                  <label>Password</label>
-                  <input class="a-input" type="password" name="password" Placeholder="Password" pattern='^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{10,}' required />
-                  <input class="a-btn" type="submit" value="Login" />
-               </form>
+   <head>
+      <link rel="stylesheet" href="index.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+   </head>
+   <body>
+      <div id="mastergrid">
+         <nav class="nav">
+            <ul>
+               <?php
+               if ($loggedIn == False) {
+                  echo
+                     '<li class="right">
+                        <button type="button"class="button" onclick=location.href="login.php">Login</button>
+                        <button type="button" class="button" onclick=location.href="signup.php">Sign Up</button>
+                     </li>';
+               } else {
+                  echo
+                     '<li class="right" >
+                        <label> Logged in as' . ' <span id="display-username">' . $_SESSION['user'] . '</span></label>
+                        <button type="button" class="button"><a href="./logout.php">Logout</a></button>
+                     </li>';
+                  header("Location: ./index.php?");
+               }
+               ?>
+            </ul>
+         </nav>
+         <!--Sign up form-->
+         <main class="main">
+            <img class="icon" src="./images/Icon.png" alt="icon">
+            <section class="outerBox">
+               <h2>Login</h2>
+               <h3>Cocktail Recipes</h3>
+               <section class="innerBox">
+                  <form action="" method="post">
+                     <label>Email</label>
+                     <input class="a-input" type="email" name="email" Placeholder="Email" pattern='^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$' required />
+                     <label>Password</label>
+                     <input class="a-input" type="password" name="password" Placeholder="Password" pattern='^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{10,}' required />
+                     <input class="a-btn" type="submit" value="Login" />
+                  </form>
+               </section>
             </section>
-         </section>
-      </main>
-   </div>
-</body>
-
+         </main>
+      </div>
+   </body>
 </html>
